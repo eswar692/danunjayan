5818e6cdf85edf1cfb8e7e84a4dcb10846d395af
eswaraprasad <esweraprasad@gmail.com>
2025-10-21 21:23:07 +0530

    name update

--- src/Genaral/secrete.tsx
+++ src/Genaral/secrete.tsx
@@ -2,7 +2,7 @@ const phone_number = "8088626694";
 const email = "";
 const whatsapp_number = "918088626694";
 const company_name = "Maha Mantrika Astro Center";
-const person_name = "Danunjayan Thantri";
+const person_name = "Pandith Dhananjayan Thanthrik";
 const address = "";
 const website_url = "https://www.mahamantrika.in";
 
